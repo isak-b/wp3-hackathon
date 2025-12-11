00405dc40aede4d0053185717c535ca3dd3b422f
Eskil Forsell <eskil.forsell@regionstockholm.se>
2025-12-11 16:54:06 +0100

    Correct async logic for ec agent

--- group1/agents/service_catalog/agent.py
+++ group1/agents/service_catalog/agent.py
@@ -54,3 +54,31 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+    tools = await client.get_tools()
+    return create_agent(
+        model=llm,
+        tools=tools
+    )
+
+
+async def main():
+    # Example of how to run the agent
+    agent = await get_agent()
+    base_messages = await client.get_prompt("service_catalog", "purpose_prompt")
+    messages = [
+        langchain.messages.SystemMessage("""\
+            Du är en hjälpsam assistent som har till uppgift att hämta och tillhandahålla 
+            information från Tjänstekatalogen. Var vänlig och hjälpsam, och ge tydlig 
+            information om tillgängliga tjänster och deras krav.
+            """),
+        * base_messages,
+        langchain.messages.HumanMessage("What services are available in the 'Productivity' category?")
+    ]
+
+    async for event in agent.astream_events({'messages': messages}):
+        print(event)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
